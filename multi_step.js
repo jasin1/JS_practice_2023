c38df16d8f4af57a40f86fc3848f6af0161e0729
jasin1 <jasin.tairaidrissi@gmail.com>
2023-02-14 13:24:47 +0100

    active first child

--- multi_step.js
+++ multi_step.js
@@ -1,7 +1,14 @@
 // test
 console.log("We are in!!");
-//again
+/*
+Eerst alle elementen verzamelen die ik nodig heb.
+Daarna eerste stap zichtbaar maken door "active" class toe te voegen
+
+*/
 const mainSteps = document.getElementsByClassName("form-step");
 const stepsIndi = document.getElementById("steps-wrapper");
+const prevBtn = document.querySelector("step-btn-prev");
+const nxtBtn = document.querySelector("step-btn-next");
 
-console.log(stepsIndi);
+mainSteps[0].classList.add("active");
+
